3460fd1be98f8a18be9e8f3662c09b650d8f263c
AhmedAGadir <ahmed.gadir@gmail.com>
2021-04-27 13:15:41 +0100

    refactored fefconfig

--- now-config/src/index.js
+++ now-config/src/index.js
@@ -12,95 +12,96 @@ const staticConfig = require('./configs/staticConfig');
 const frontEndFrameworkConfig = require('./configs/frontEndFrameworkConfig');
 
 async function buildConfig() {
-  let config = {
-    version: 2,
-  };
+    let config = {
+        version: 2,
+    };
 
-  const answers = await inquirer
-    .prompt([
-      {
-        type: 'text',
-        name: 'name',
-        message: 'What is the name of the project? 🤔',
-        default: path.basename(process.cwd()),
-      },
-      {
-        type: 'list',
-        name: 'type',
-        message: 'What type of project? 📦',
-        choices: ['node-express', 'static', 'react', 'vue', 'static-build'],
-      },
-    ]);
+    const answers = await inquirer
+        .prompt([
+            {
+                type: 'text',
+                name: 'name',
+                message: 'What is the name of the project? 🤔',
+                default: path.basename(process.cwd()),
+            },
+            {
+                type: 'list',
+                name: 'type',
+                message: 'What type of project? 📦',
+                choices: ['node-express', 'static', 'react', 'vue', 'static-build'],
+            },
+        ]);
 
-  config.name = answers.name;
-  switch (answers.type) {
-    case 'node-express':
-      config = await nodeExpressConfig(config);
-      break;
-    case 'static':
-      config = await staticConfig(config);
-      break;
-    case 'react':
-      config = await frontEndFrameworkConfig(config, 'build');
-      break;
-    case 'vue':
-      config = await frontEndFrameworkConfig(config, 'dist');
-      break;
-    case 'static-build':
-      config = await frontEndFrameworkConfig(config, 'dist');
-      break;
-    default:
-      break;
-  }
-  const moreAnswers = await inquirer
-    .prompt([
-      {
-        type: 'confirm',
-        name: 'specifyAlias',
-        message: 'Would you like to specify an alias? 👻',
-        default: true,
-      },
-      {
-        type: 'text',
-        name: 'alias',
-        message: 'What is the alias? 🕵🏾‍♂️',
-        default: answers.name,
-        when: (a) => a.specifyAlias === true,
-      },
-      {
-        type: 'confirm',
-        name: 'deploy',
-        message: 'Would you like to deploy now? 👨🏾',
-        default: false,
-      },
-    ]);
+    config.name = answers.name;
+    switch (answers.type) {
+        case 'node-express':
+            config = await nodeExpressConfig(config);
+            break;
+        case 'static':
+            config = await staticConfig(config);
+            break;
+        case 'react':
+            config = await frontEndFrameworkConfig(config, 'build');
+            break;
+        case 'vue':
+            config = await frontEndFrameworkConfig(config, 'dist');
+            break;
+        case 'static-build':
+            config = await frontEndFrameworkConfig(config, 'dist');
+            break;
+        default:
+            break;
+    }
+    const moreAnswers = await inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'specifyAlias',
+                message: 'Would you like to specify an alias? 👻',
+                default: true,
+            },
+            {
+                type: 'text',
+                name: 'alias',
+                message: 'What is the alias? 🕵🏾‍♂️',
+                default: answers.name,
+                when: (a) => a.specifyAlias === true,
+            },
+            {
+                type: 'confirm',
+                name: 'deploy',
+                message: 'Would you like to deploy now? 👨🏾',
+                default: false,
+            },
+        ]);
 
-  config.alias = moreAnswers.alias;
-  fs.writeFileSync(nowPath, JSON.stringify(config, null, 2), 'utf-8');
+    config.alias = moreAnswers.alias;
+    fs.writeFileSync(nowPath, JSON.stringify(config, null, 2), 'utf-8');
 
-  if (moreAnswers.deploy) {
-    console.log('deploying 🚀...');
-    console.log('All done! 🎉🎉');
-  }
+    if (moreAnswers.deploy) {
+        console.log('deploying 🚀...');
+        // not implemented
+        console.log('All done! 🎉🎉');
+    }
 }
 
 if (existingConfig) {
-  inquirer
-    .prompt([
-      {
-        type: 'confirm',
-        name: 'overwrite',
-        message: '🚫 🚨 now.json already exists! Would you like to overwrite it? 🚫 🚨',
-        default: false,
-      },
-    ])
-    .then((answers) => {
-      if (answers.overwrite) {
-        buildConfig();
-      } else {
-        console.log('Goodbye! 👋🏾');
-      }
-    });
+    inquirer
+        .prompt([
+            {
+                type: 'confirm',
+                name: 'overwrite',
+                message: '🚫 🚨 now.json already exists! Would you like to overwrite it? 🚫 🚨',
+                default: false,
+            },
+        ])
+        .then((answers) => {
+            if (answers.overwrite) {
+                buildConfig();
+            } else {
+                console.log('Goodbye! 👋🏾');
+            }
+        });
 } else {
-  buildConfig();
+    buildConfig();
 }
