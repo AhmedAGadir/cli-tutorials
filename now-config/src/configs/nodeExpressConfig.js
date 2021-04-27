e2f73504674023b05f410c11b429797b23f20b10
AhmedAGadir <ahmed.gadir@gmail.com>
2021-04-27 13:05:56 +0100

    refactored fefconfig

--- now-config/src/configs/nodeExpressConfig.js
+++ now-config/src/configs/nodeExpressConfig.js
@@ -2,45 +2,45 @@ const path = require('path');
 const inquirer = require('inquirer');
 
 const baseConfig = {
-    builds: [
-        {
-            src: 'src/index.js',
-            use: '@now/node-server',
-        },
-    ],
-    routes: [
-        { src: '/.*', dest: 'src/index.js' },
-    ],
+  builds: [
+    {
+      src: 'src/index.js',
+      use: '@now/node-server',
+    },
+  ],
+  routes: [
+    { src: '/.*', dest: 'src/index.js' },
+  ],
 };
 
 async function nodeExpressConfig(config) {
-    let mainFile = 'src/index.js'
-    try {
-        // eslint-disable-next-line
+  let mainFile = 'src/index.js';
+  try {
+    // eslint-disable-next-line
         const packageJSON = require(path.join(process.cwd(), 'package.json'))
-        mainFile = packageJSON.main;
-        // eslint-disable-next-line
+    mainFile = packageJSON.main;
+    // eslint-disable-next-line
     } catch (error) {
-        // swallowing this error 
-    }
+    // swallowing this error
+  }
 
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'text',
-                name: 'name',
-                message: 'What is the path to your express entry point?',
-                default: mainFile,
-            },
-        ]);
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'text',
+        name: 'name',
+        message: 'What is the path to your express entry point? 🦠',
+        default: mainFile,
+      },
+    ]);
 
-    baseConfig.builds[0].src = answers.name;
-    baseConfig.routes[0].destination = answers.name;
+  baseConfig.builds[0].src = answers.name;
+  baseConfig.routes[0].destination = answers.name;
 
-    return {
-        ...config,
-        ...baseConfig
-    };
+  return {
+    ...config,
+    ...baseConfig,
+  };
 }
 
 module.exports = nodeExpressConfig;
--- now-config/src/configs/staticConfig.js
+++ now-config/src/configs/staticConfig.js
@@ -2,30 +2,30 @@ const path = require('path');
 const inquirer = require('inquirer');
 
 const baseConfig = {
-    builds: [
-        {
-            src: '*',
-            use: '@now/node-static',
-        },
-    ]
+  builds: [
+    {
+      src: '*',
+      use: '@now/node-static',
+    },
+  ],
 };
 
 async function staticConfig(config) {
-    const answers = await inquirer
-        .prompt([
-            {
-                type: 'text',
-                name: 'directory',
-                message: 'What folder would you like to deploy',
-                default: '.',
-            },
-        ]);
+  const answers = await inquirer
+    .prompt([
+      {
+        type: 'text',
+        name: 'directory',
+        message: 'What folder would you like to deploy? 🔎',
+        default: '.',
+      },
+    ]);
 
-    baseConfig.builds[0].src = path.join(answers.directory, '*');
-    return {
-        ...config,
-        ...baseConfig
-    };
+  baseConfig.builds[0].src = path.join(answers.directory, '*');
+  return {
+    ...config,
+    ...baseConfig,
+  };
 }
 
 module.exports = staticConfig;
